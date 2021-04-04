22bbd7e4c162116f6ff4ef6721156730e5043410
Vit <my_message_ru@mail.ru>
2021-04-05 00:33:10 +0300

    Реалезация выбора изображений

--- views/article/_form.php
+++ views/article/_form.php
@@ -3,10 +3,13 @@
 use app\models\Article;
 
 //use kartik\date\DatePicker;
+use yii\helpers\FileHelper;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\jui\DatePicker;
 use yii\web\View;
 use yii\widgets\ActiveForm;
+use kartik\file\FileInput;
 
 /* @var $this View */
 /* @var $model Article */
@@ -29,12 +32,29 @@ use yii\widgets\ActiveForm;
             'dateFormat' => 'dd.MM.yyyy',
         ]) ?>
     <div class="row block-add-image">
-        <div class="col-md-2">
-            <?= $form->field($model, 'detailPicture')->fileInput() ?>
+<!--        <div class="">-->
+<!--            --><?//= $form->field($model, 'detailPicture')->fileInput() ?>
+<!---->
+<!--        </div>-->
+        <div class="">
+            <?= $form->field($model, 'detailPicture')->widget(FileInput::class, [
+                'options' => [
+//                  'accept' => 'image'
+                    'multiple' => true
+                ],
+                'pluginOptions'=>[
+                    'deleteUrl' => Url::toRoute(['realty/delete-image']),
+                    'allowedFileExtensions'=>['jpg','gif','png'],
+                    'showUpload' => false,
+                    'overwriteInitial'=>false,
+                    ],
+             ]); ?>
+
         </div>
-        <div class="col-md-2">
+        <div class="">
             <?= $form->field($model, 'previewPicture')->fileInput() ?>
         </div>
+
     </div>
         <div class="form-group">
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
@@ -42,3 +62,39 @@ use yii\widgets\ActiveForm;
     <?php ActiveForm::end(); ?>
 
 </div>
+
+<hr>
+<?php
+
+$files= FileHelper::findFiles('img/detail/');
+$arr = [];
+if (isset($files[0])) {
+    foreach ($files as $index => $file) {
+        $nameFile = substr($file, strrpos($file, '/') + 1);
+//        echo Html::a($nameFile, Url::base().'/img/detail'.$nameFile);
+        $arr[] = 'http://test.local/img/detail/' . $nameFile; // render do ficheiro no browser
+    }
+} else {
+    echo "Изображения отсутствуют!";
+}
+debug($arr);
+
+echo FileInput::widget([
+    'name' => 'attachment_49[]',
+    'options'=>[
+        'multiple'=>true
+    ],
+    'pluginOptions' => [
+        'deleteUrl' => Url::toRoute(['realty/delete-image']),
+        'initialPreview' => $arr,
+        'initialPreviewAsData'=>true,
+        'initialCaption'=>"The Moon and the Earth",
+        'initialPreviewConfig' => [
+            ['caption' => 'Moon.jpg', 'size' => '873727'],
+            ['caption' => 'Earth.jpg', 'size' => '1287883'],
+        ],
+        'overwriteInitial'=>false,
+        'maxFileSize'=>1000
+    ]
+]);
+?>
