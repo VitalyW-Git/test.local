7925d87ef3c84cd50734563e736bd25d6f4e3fa9
Vitaliy <vitalik_v@modimio.ru>
2021-09-01 18:10:01 +0300

    Построение запросов

--- controllers/ConnectController.php
+++ controllers/ConnectController.php
@@ -14,31 +14,6 @@ use yii\web\Controller;
 
 class ConnectController extends Controller
 {
-    /**
-     * @throws VkException
-     */
-    public function actionData()
-    {
-        $token = 'b842cc1786c634e1da39bb2f7b72ed213c0e288841b6f506d72df16b09ad10f84462dbd1a102113e47c21';
-        $photo = '[{\"markers_restarted\":true,\"photo\":\"bf6b238ee2:x\",\"sizes\":[],\"latitude\":0,\"longitude\":0,\"kid\":\"8da9733487038987e371b73b9bb1f8e0\",\"sizes2\":[[\"s\",\"ba320d70d2b77d831e24c8af066dfc835540c2c12c23e7af906b2ba1\",\"426591580646040240\",75,75],[\"m\",\"28af227bededf52cca07f008211cf4cc05159d0268fb96774e5ebe64\",\"9018979779236483158\",130,130],[\"x\",\"11b27de413901327e6f45b013c4627c7bb5b3a2039a5d3752f2a437d\",\"3591792707075910925\",520,520],[\"o\",\"28af227bededf52cca07f008211cf4cc05159d0268fb96774e5ebe64\",\"9018979779236483158\",130,130],[\"p\",\"93fceb1ab23af3c615ff1ae88f86d68e1eedd9d4e691757c6c7c340e\",\"4954993403952573361\",200,200],[\"q\",\"1589cd0268c257f26d94f6a3ff9557b566e6bd8dcc2a5d768c1b27da\",\"-8845168586407118357\",320,320],[\"r\",\"44cc4dd93e48812c369ee20bdf11ee56c964e130d3b9c667dc4ac503\",\"8146435985550694322\",510,510]],\"urls\":[],\"urls2\":[\"ujINcNK3fYMeJMivBm38g1VAwsEsI-evkGsroQ/sGKUTMiO6wU.jpg\",\"KK8ie-3t9SzKB_AIIRz0zAUVnQJo-5Z3Tl6-ZA/VlDSGVPaKX0.jpg\",\"EbJ95BOQEyfm9FsBPEYnx7tbOiA5pdN1LypDfQ/DS0Z3G-c2DE.jpg\",\"KK8ie-3t9SzKB_AIIRz0zAUVnQJo-5Z3Tl6-ZA/VlDSGVPaKX0.jpg\",\"k_zrGrI688YV_xroj4bWjh7t2dTmkXV8bHw0Dg/sSv_ij-sw0Q.jpg\",\"FYnNAmjCV_JtlPaj_5VXtWbmvY3MKl12jBsn2g/63nCzAimP4U.jpg\",\"RMxN2T5IgSw2nuIL3xHuVslk4TDTucZn3ErFAw/slc1AF70DXE.jpg\"]}]';
-        $hash = 'f44fb936a35389186d778861e57600ea';
-        $idApp = '20618894';
-        $idCategory = '200064586';
-//        $queryProduct = 'https://api.vk.com/method/market.get?owner_id=-'.$idApp.'&album_id=20&count=2&access_token='.$token.'&v=5.125';
-//        $getProduct = 'https://api.vk.com/method/market.get?owner_id=-'.$idApp.'&album_id=20&count=2&access_token='.$token.'&v=5.125';
-        $imgProduct = 'https://api.vk.com/method/photos.getMarketUploadServer?group_id=200064586&access_token='.$token.'&v=5.125';
-        $saveProduct = 'https://api.vk.com/method/photos.saveMarketPhoto?group_id=200064586&photo='.$photo.'&server=844129&hash='.$hash.'&access_token='.$token.'&v=5.131';
-//        $addProduct = 'https://api.vk.com/method/market.add?owner_id=-'.$idCategory.'&main_photo_id=457239461&name=testing&description=testTestTestTestTest&category_id=901&access_token='.$token.'&v=5.152';
-        $getData = file_get_contents($saveProduct);
-//        $c = json_decode($getData);
-//        $g = $c->response->upload_url;
-//        $a = "<form action='$g' enctype='multipart/form-data' method='post'>
-//            <input type='file' name='file'>
-//            <Button type='submit'>Button</Button>
-//        </form>";
-        VarDumper::dump(json_decode($getData),11,true);
-//        echo $a;
-    }
 
     public function actionGetCategory()
     {
@@ -97,7 +72,6 @@ class ConnectController extends Controller
                     $buf[] = 'title='.urlencode($arr[$key]['name']);
                     $buf[] = 'access_token='.$access_token;
                     if ($arr[$key]['img'] <> '') {
-
 // Запрашиваем ссылку для загрузки фотографии
                         $url = $url_vk.'photos.getMarketAlbumUploadServer?group_id='.$owner_id.'&access_token='.$access_token;
                         echo '<br>'.$url;
@@ -144,7 +118,7 @@ class ConnectController extends Controller
                 }
                 foreach ($arr[$key]['goods'] as $key2 => $value2) {
                     if ($arr[$key]['goods'][$key2]['vk_market_item_id'] == '') {
-// добавляем товар
+// запрос на добавление товара
                         $buf = [];
                         $buf[] = 'owner_id=-'.$owner_id;
                         $buf[] = 'name='.urlencode($arr[$key]['goods'][$key2]['name']);
@@ -261,13 +235,15 @@ class ConnectController extends Controller
     }
 
 
-    public function actionModel()
+    public function actionAddProduct()
     {
-        $group_id = "200064586";
+        $group_id = "20618894"; // owner_id, [market_item_id] => 5400440
         $album_id = '277304408';
+        $owner_id = '-20618894';
         $v = 5.131;
-        $file = '@'.$_SERVER['DOCUMENT_ROOT'] . '/uploads/gm.jpg';
-        var_dump($file);die();
+        $file = $_SERVER['DOCUMENT_ROOT'] . '/uploads/gm1.jpg';
+        $buf = [];
+
         $params = [];
         if ($group_id) {
             $params['group_id'] = $group_id;
@@ -279,25 +255,17 @@ class ConnectController extends Controller
         //Получаем сервер для загрузки изображения
         $response = $this->method("photos.getMarketUploadServer", $params);
 
-
         if (isset($response) == false) {
             print_r($response);
             exit;
         }
         $server = $response->response->upload_url;
 
+        $json = $this->dispatchImageOnServerVk($file, $server);
 
-//        $postparam = ["file1" => $file];
-        //Отправляем файл на сервер
-        $ch = curl_init($server);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data; charset=UTF-8'));
-        $json = json_decode(curl_exec($ch));
-        curl_close($ch);
 
-        //Сохраняем файл в альбом
+
+        //Сохраняем изображение на сервере
         $photo = $this->method("photos.saveMarketPhoto", [
                 "group_id" => $group_id,
                 "photo" => $json->photo,
@@ -306,13 +274,33 @@ class ConnectController extends Controller
                 "v" => $v,
             ]
         );
-        VarDumper::dump($photo,11,true);
-//        VarDumper::dump($photo,11,true);
+        VarDumper::dump($photo->response[0]->id,11,true);
+
         if (isset($photo->response[0]->id)) {
-            return $photo->response[0]->id;
+            $buf['main_photo_id'] = $photo->response[0]->id;
         } else {
             return false;
         }
+
+        // Добавляем товара
+        $prod = $this->method("market.add", [
+                "owner_id" => $owner_id,
+                "name" => 'testTest',
+                "description" => '\nЗаказать через сайт:https:\n//model-lavka.ru/cat/trains/16790.html?r1=yandext&r2=',
+                "category_id" => '902',
+                "price" => '555',
+                "main_photo_id" => $buf['main_photo_id'],
+                "v" => $v,
+            ]
+        );
+        sleep(3);
+        VarDumper::dump($photo->response[0]->id,11,true);
+        VarDumper::dump($prod,11,true);
+    }
+
+    public function actionAddNewAlbum()
+    {
+
     }
 
     /**
@@ -340,16 +328,20 @@ class ConnectController extends Controller
         return false;
     }
 
-
-    public function actionAlbom()
+    public function dispatchImageOnServerVk($file, $server)
     {
-//      фокусы
-        $owner_id = 200064586;
-        $album_ids = 279884086;
-        $token = 'b842cc1786c634e1da39bb2f7b72ed213c0e288841b6f506d72df16b09ad10f84462dbd1a102113e47c21';
-        $saveProduct = 'https://api.vk.com/method/photos.getAlbums?owner_id=-'.$owner_id.'&album_ids='.$album_ids.'&access_token='.$token.'&v=5.131';
-        $getData = file_get_contents($saveProduct);
-        VarDumper::dump(json_decode($getData),11,true);
+        //Отправляем изображение на сервер
+        $file = curl_file_create($file,'image/jpeg','gm1.jpg');
+        $ch = curl_init($server);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ["file" => $file]);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data; charset=UTF-8']);
+        sleep(3);
+        $exec = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($exec);
     }
+
 
 }
